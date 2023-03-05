64b639189e75bbf6356cc8c2de1762a09b2eadaa
Musayeroh Bah <mbah@smith.edu>
2023-03-05 12:45:13 -0500

    Update Engine.java

--- Engine.java
+++ Engine.java
@@ -1,3 +1,27 @@
 public class Engine {
+    private FuelType fuel_type;
+    private double maximum_fuel_level;
+    public double current_fuel_level;
+
+    public Engine(FuelType fuel_type, double maximum_fuel_level, double current_fuel_level){
+        this.fuel_type = fuel_type;
+        this.maximum_fuel_level = maximum_fuel_level;
+        this.current_fuel_level = current_fuel_level;
+    }
+
+    public void go() {
+            if (this.current_fuel_level > 0) {
+                this.current_fuel_level--;
+                System.out.println(current_fuel_level);
+            }
+            else{
+                throw new RuntimeException("Out of fuel!");
+            }
+    }
+
+    public void refuel() {
+        //make boolean statement for when current fuel level is already equal to the max fuel level?
+        this.current_fuel_level = this.maximum_fuel_level;
+    }
 
 }
