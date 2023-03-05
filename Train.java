8aee0df2f9bef50e762a2f1d384ca35bb124ac54
Musayeroh Bah <mbah@smith.edu>
2023-03-05 12:47:06 -0500

    Update Train.java

--- Train.java
+++ Train.java
@@ -1,3 +1,14 @@
 public class Train {
+    public static void main(String[] args) {
 
+        Engine myEngine = new Engine(FuelType.ELECTRIC, 100.0, 15.0);
+        try {
+            while (true) {
+                myEngine.go();
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage()); // Out of fuel (what is this line supposed to do after throwing the exception in engine.java?)
+        }
+    }
 }
+
